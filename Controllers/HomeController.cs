06e584ef68d80916709ff7334155a594d5acaa69
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-19 13:31:33 -0800

    Add a Views folder that contains html that MVC uses. Alters route decorator and data type in the controller to return html wihtout typing out a string and rendering it with a produce route decorator

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,28 +10,10 @@ namespace FriendLeter.Controllers
     [Route("/goodbye")]
     public string Goodbye() { return "Goodbye friend";}
 
-    [Produces("text/html")]
     [Route("/")]
-    public string Letter()
+    public ActionResult Letter()
     {
-      return
-      "<!DOCTYPE html>" +
-      "<html>" +
-      "<head>" +
-        "<title>Hello Friend!</title>" +
-        "<link rel='stylesheet' href='https://maxcdn.bootstrap.cdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-      "</head>" +
-      "<body>" +
-        "<h1>Hello From Afar</h1>" +
-        "<p>Dear Josh,</p>" +
-        "<p>How are you? I hope that you are having a nice weekend! I am learning programming while in Iceland! </p>" +
-        "<p>Josh, you would not believe how cold it is here. I probably should have gone to Hawaii instead </p>" +
-        "<p>But I like programming a lot, so I've got that going for me. </p>" +
-        "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-        "<p>Cheers,</p>" +
-        "<p>Travel Enthusiast Chris</p>" +
-        "</body>" +
-        "</html>";
+      return View();
     }
   }
 }
